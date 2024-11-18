1e72cf6dd6bb242670c79768fe859bd3c646baa7
randriamiharyDani <randriamiharySteev@gmail.com>
2024-11-18 15:13:01 +0300

    ui:create accueilComponent with route &&install package

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { AccueilComponent } from './page/accueil/accueil.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: AccueilComponent,
+    pathMatch: 'full'
+
+  }
+];
