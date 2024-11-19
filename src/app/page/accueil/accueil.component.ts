92259ac7f005f02b677c93d0f0c583c7721da3cf
randriamiharyDani <randriamiharySteev@gmail.com>
2024-11-19 11:47:16 +0300

    ui: instal tailwind &&headercomponent

--- src/app/page/accueil/accueil.component.ts
+++ src/app/page/accueil/accueil.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from "../../components/header/header.component";
 
 @Component({
   selector: 'app-accueil',
   standalone: true,
-  imports: [],
+  imports: [HeaderComponent],
   templateUrl: './accueil.component.html',
   styleUrl: './accueil.component.scss'
 })
